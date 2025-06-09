95fd5eb4383044aab385bcb2d51fd6af77e997a3
egidomireia <egidomireia@gmail.com>
2025-06-09 17:20:29 +0200

    Create __init__.py

---
+++ gwresponse/__init__.py
@@ -0,0 +1,9 @@
+"""
+gwresponse: Gravitational wave response tools for LISA and waveform modeling.
+"""
+
+# Import key classes/functions for easier access
+from .orbit import LISA_spacecraft
+from .waveform import IMRPhenomD_JAX
+from .response import LISA_response
+
